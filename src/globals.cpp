16c84e55721a80220d3a22488e2c1493f879d31e
T3M1N4L <108430379+T3M1N4L@users.noreply.github.com>
2026-03-13 03:18:46 -0500

    uhh ok angular

--- src/globals.cpp
+++ src/globals.cpp
@@ -81,17 +81,17 @@ lemlib::ControllerSettings lateral_controller(15,  // proportional gain (kP)
                                               50,  // small error range timeout, in milliseconds
                                               3,   // large error range, in inches
                                               200, // large error range timeout, in milliseconds
-                                              35   // maximum acceleration (slew)
+                                              0   // maximum acceleration (slew)
 );
 
 // angular PID controller
-lemlib::ControllerSettings angular_controller(6,      // proportional gain (kP)
-                                              0, // integral gain (kI)
-                                              0,     // derivative gain (kD)
-                                              3,      // anti windup
-                                              1,      // small error range, in inches
-                                              25,     // small error range timeout, in milliseconds
-                                              3,      // large error range, in inches
+lemlib::ControllerSettings angular_controller(4.3,      // proportional gain (kP)
+                                              0.01, // integral gain (kI)
+                                          15,     // derivative gain (kD)
+                                              0,      // anti windup
+                                              0.25,      // small error range, in inches
+                                              100,     // small error range timeout, in milliseconds
+                                          2,      // large error range, in inches
                                               100,    // large error range timeout, in milliseconds
                                               0       // maximum acceleration (slew)
 );
